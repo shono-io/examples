34e93d7a59a222f2a254b0177fb9c872101f1b0e
Daan Gerits <dgerits@korfinancial.com>
2023-05-11 22:41:12 +0200

    refactored the codebase to make use of memphis. Added abstractions for the reaktors to be used with memphis or kafka

--- todos/events.go
+++ todos/events.go
@@ -9,9 +9,9 @@ var (
 	space   = "todos"
 	concept = "todo"
 
-	TaskAdded    = go_shono.NewEvent(go_shono.NewEventId(org, space, concept, "added"), new(TaskAddedEvent))
-	TaskFinished = go_shono.NewEvent(go_shono.NewEventId(org, space, concept, "finished"), new(TaskFinishedEvent))
-	TaskDeleted  = go_shono.NewEvent(go_shono.NewEventId(org, space, concept, "deleted"), new(TaskDeletedEvent))
+	TaskAdded    = go_shono.NewEvent(go_shono.NewEventId(org, space, concept, "added"), new(TaskAddedEvent), nil)
+	TaskFinished = go_shono.NewEvent(go_shono.NewEventId(org, space, concept, "finished"), new(TaskFinishedEvent), nil)
+	TaskDeleted  = go_shono.NewEvent(go_shono.NewEventId(org, space, concept, "deleted"), new(TaskDeletedEvent), nil)
 )
 
 type TaskAddedEvent struct {
